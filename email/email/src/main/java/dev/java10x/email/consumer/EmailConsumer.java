788170f375f199aa742c7601b7252b83770a3189
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-17 20:55:28 -0300

    feat: estruturando a config do rabbit e o tipo consumer

--- email/email/src/main/java/dev/java10x/email/consumer/EmailConsumer.java
+++ email/email/src/main/java/dev/java10x/email/consumer/EmailConsumer.java
@@ -1,4 +1,14 @@
 package dev.java10x.email.consumer;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+
+@Component
 public class EmailConsumer {
+    @RabbitListener(queues = "email-queue")
+    public void listenEmail(@Payload String string){
+        System.out.println(string);
+    }
 }
