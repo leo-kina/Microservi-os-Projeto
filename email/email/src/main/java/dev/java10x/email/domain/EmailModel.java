a648b936c9962813c29cdb031cce79bd06f5e21a
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-17 20:54:34 -0300

    refactor: refatorando model adiconando id e tipo UUID

--- email/email/src/main/java/dev/java10x/email/domain/EmailModel.java
+++ email/email/src/main/java/dev/java10x/email/domain/EmailModel.java
@@ -1,14 +1,13 @@
 package dev.java10x.email.domain;
 
 import dev.java10x.email.enums.EmailStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @Table(name = "TB_EMAIL")
@@ -16,13 +15,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class EmailModel {
-
-    private String emailId;
-    private String userId;
+    private final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID emailId;
+    private UUID userId;
     private String emailFrom;
     private String emailTo;
     private String emailSubject;
-    @Column(columnDefinition = "BODY")
+    @Column(columnDefinition = "TEXT")
     private String Body;
     private LocalDateTime sendDateTime;
     private EmailStatus statusEmail;
