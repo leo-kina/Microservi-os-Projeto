a7f5ad9ba53d29da4b10caabe8ce1dcf73d110e1
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-09-22 12:42:32 -0300

    feat: Criando UserDto e usando validacoes

--- user/user/src/main/java/dev/java10x/user/domain/UserModel.java
+++ user/user/src/main/java/dev/java10x/user/domain/UserModel.java
@@ -10,12 +10,40 @@ import java.util.UUID;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
 @Table(name = "TB_USERS")
 public class UserModel {
+    private final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID UserID;
     private String name;
     private String email;
+
+    public long getSerialVersionUID() {
+        return serialVersionUID;
+    }
+
+    public UUID getUserID() {
+        return UserID;
+    }
+
+    public void setUserID(UUID userID) {
+        UserID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
