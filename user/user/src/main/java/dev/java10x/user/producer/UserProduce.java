6bf56d4f1561010d9cde67464eee173d179244ac
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-10-08 19:09:31 -0300

    feat: Rabbit template

--- user/user/src/main/java/dev/java10x/user/producer/UserProduce.java
+++ user/user/src/main/java/dev/java10x/user/producer/UserProduce.java
@@ -1,4 +1,18 @@
 package dev.java10x.user.producer;
 
+import dev.java10x.user.domain.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
 public class UserProduce {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProduce(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendUserCreatedEvent(UserModel userModel){
+        rabbitTemplate.convertAndSend("user.created", event);
+    }
+
 }
