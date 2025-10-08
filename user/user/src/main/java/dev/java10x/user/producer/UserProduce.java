793d5ccb93af700a91e5fc21421c99e789255ee6
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-10-08 19:57:59 -0300

    featadd rabbitTemplate functions

--- user/user/src/main/java/dev/java10x/user/producer/UserProduce.java
+++ user/user/src/main/java/dev/java10x/user/producer/UserProduce.java
@@ -1,18 +1,36 @@
 package dev.java10x.user.producer;
 
 import dev.java10x.user.domain.UserModel;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import dev.java10x.user.dto.EmailDto;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserProduce {
 
     final RabbitTemplate rabbitTemplate;
 
+    private final String routingKey = "email-queue";
+
+
+
     public UserProduce(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
+
     }
 
-    public void sendUserCreatedEvent(UserModel userModel){
-        rabbitTemplate.convertAndSend("user.created", event);
+    public void publishEvent(UserModel userModel){
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userModel.getUserID());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setEmailSubject("Bem vindo");
+        emailDto.setBody("helloo" + userModel.getName());
+                rabbitTemplate.convertAndSend(
+               "",
+               routingKey,
+               emailDto
+       );
     }
 
 }
